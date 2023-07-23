7e9697ba1021c98682954d026b17963a8a71fcf3
EdIvarsson413 <EadIvarsson413>
2023-07-23 13:20:33 -0600

    Modo Oscuro agregado - arreglado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,23 @@
 import { useEffect, useState } from 'react'
 import LightIcon from '@/components/icons/LightIcon'
 import DarkIcon from '@/components/icons/DarkIcon'
+import { error } from 'console'
 
 // Configuración para traer el modo oscuro del sistema
 const modoInicial = () => {
-    if( localStorage.getItem( "theme" ) ) {
-        return localStorage.getItem( "theme" ) as "light" | "dark";
+    if( typeof window !== "undefined" ) {
+        if( localStorage.getItem( "theme" ) ) {
+            return localStorage.getItem( "theme" ) as "light" | "dark";
+        }
+        return window.matchMedia( "(prefers-color-scheme: dark)" ).matches ? "dark" : "light";
     }
-    return window.matchMedia( "(prefers-color-scheme: dark)" ).matches ? "dark" : "light";
+    return "light"
 }
 
 const Navbar = () => {
     // Se carga el tema en un string
     const [ modo, setModo] = useState<"light" | "dark">(modoInicial);
+    const [ advertencia, setAdvertencia ] = useState<boolean>(false);
 
     // Se actualiza constantemente el tema con la varibale de localStorage y el modo de clases de Tailwinds
     useEffect(() => {
@@ -25,21 +30,30 @@ const Navbar = () => {
 
     const handleTheme = () => modo === 'light' ? setModo( "dark" ) : setModo( "light" );
 
+    const aviso = () => { setAdvertencia( true ); setTimeout(() => { setAdvertencia( false ) }, 1500 ); }
+
     return (
-        <header className="flex justify-between items-center  mb-12 text-white">
-            <h1 className="text-2xl font-semibold dark:text-white text-black">devfinder</h1>
-            <button className="flex items-center gap-3 uppercase" onClick={handleTheme}>
-                <p className='dark:text-white text-black'>{ modo === 'light' ? 'Oscuro' : 'Claro' }</p>  
-                <i>
-                    {
-                        modo === 'light' ?
-                        <DarkIcon width={27} height={27} className='fill-black'/>
-                        :
-                        <LightIcon width={27} height={27} className='fill-white'/> 
-                    }
-                </i>
-            </button>
-        </header> 
+        <>
+            <header className="flex justify-between items-center mb-3 text-white">
+                <h1 className="text-2xl font-semibold dark:text-white text-black">devfinder</h1>
+                <button className="flex items-center gap-3 uppercase" onClick={aviso}>
+                    <p className='dark:text-white text-black'>{ modo === 'light' ? 'Oscuro' : 'Claro' }</p>  
+                    <i>
+                        {
+                            modo === 'light' ?
+                            <DarkIcon width={27} height={27} className='fill-black'/>
+                            :
+                            <LightIcon width={27} height={27} className='fill-white'/> 
+                        }
+                    </i>
+                </button>
+            </header>
+
+            <section className={`mb-12 text-center font-semibold rounded-xl ${advertencia && 'p-2'} 
+                                bg-gradient-to-br from-orange-200 to-yellow-400`}>
+                { advertencia && 'La opción de tema aún no funciona' }
+            </section>
+        </>
     )
 }
 
